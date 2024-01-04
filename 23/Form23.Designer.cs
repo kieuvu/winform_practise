a5ff40caae5b99fdb2acc1b8351c6108906f4f67
kieuvu <kieuvuvuvu@gmail.com>
2024-01-04 23:17:05 +0700

    update

--- 23/Form23.Designer.cs
+++ 23/Form23.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            splitContainer1 = new SplitContainer();
+            listBox1 = new ListBox();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)this.webView21).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(183, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Thêm đường dẫn";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(287, 18);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(359, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(346, 47);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += this.button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(427, 47);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Cập nhật";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += this.button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(508, 47);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "Xóa";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += this.button3_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(1, 76);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(this.webView21);
+            splitContainer1.Size = new Size(856, 372);
+            splitContainer1.SplitterDistance = 285;
+            splitContainer1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(0, 0);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(285, 372);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += this.listBox1_SelectedIndexChanged;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = Color.White;
+            this.webView21.Dock = DockStyle.Fill;
+            this.webView21.Location = new Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new Size(567, 372);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // Form23
+            // 
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(860, 451);
+            this.Controls.Add(splitContainer1);
+            this.Controls.Add(button3);
+            this.Controls.Add(button2);
+            this.Controls.Add(button1);
+            this.Controls.Add(textBox1);
+            this.Controls.Add(label1);
+            this.Name = "Form23";
             this.Text = "Form23";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)this.webView21).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private SplitContainer splitContainer1;
+        private ListBox listBox1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
     }
 }
--- 23/Form23.cs
+++ 23/Form23.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WinFormsApp1._23
 {
@@ -15,6 +16,47 @@ namespace WinFormsApp1._23
         public Form23()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            if (string.IsNullOrEmpty(url) || string.IsNullOrWhiteSpace(url)) return;
+
+            listBox1.Items.Add(url);
+            textBox1.Text = string.Empty;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                string selectedItem = listBox1.SelectedItem.ToString();
+                textBox1.Text = selectedItem;
+                webView21.Source = new Uri(selectedItem);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            if (string.IsNullOrEmpty(url) || string.IsNullOrWhiteSpace(url)) return;
+            if (listBox1.SelectedIndex == -1) return;
+
+            listBox1.Items[listBox1.SelectedIndex] = url;
+            webView21.Source = new Uri(url);
+            textBox1.Text = string.Empty;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                int selectedIndex = listBox1.SelectedIndex;
+                listBox1.Items.RemoveAt(selectedIndex);
+                listBox1.SelectedIndex = -1;
+                webView21.CoreWebView2.Navigate("about:blank");
+            }
         }
     }
 }
